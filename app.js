b65ee9d99dc689807ddd0f17fa21ba55f261ddaa
Kirti Sharma <kirti2001sharma@gmail.com>
2023-01-26 14:27:02 +0530

    Feat: added audio for tiles

--- app.js
+++ app.js
@@ -1,34 +1,75 @@
 const tiles = document.querySelectorAll(".tile") //all colors
 const startBtn = document.querySelector(".start-btn")
 const info = document.querySelector(".info") // instructions
+const audio  = document.querySelector(".audio")
 
-// to start the game
-const start = () => {
-    info.classList.remove("hidden")
-    info.textContent = "Wait for the computer!"
+let canClick = false; 
+
+const getRandomTiles = () => {
+    return tiles[parseInt(Math.random()*tiles.length)]
 }
 
-startBtn.addEventListener("click", () => {
-    info.classList.remove("hidden")
-    info.textContent = "Wait for the computer!"
-})
+
+const computerSequence = [getRandomTiles()]
+let computerSequenceCopy= [...computerSequence]
+let userTurnCount = computerSequence.length;
+
 // console.log(startBtn)
 
 const flash = (tile) => {
     return new Promise((resolve, reject) => {
+        // first setTimeout: to have some delay between one user tap and next computer flash, 500ms
+        setTimeout(()=>{
         tile.classList.add("active");
+        audio.play()
         setTimeout(() => {
             tile.classList.remove("active")
-            resolve();
+            
+            //delay between flashing of two tiles
+            setTimeout(() => {
+                resolve();
+            }, 250)
         }, 1000)
-        
+        }, 500)   
     })
 }
 
-const flashRunner = async () => {
-    for(const tile of tiles){
+const flashRunner = async (computerSequenceCopy) => {
+    info.textContent = "Wait for Computer"
+    canClick = false; // user can't click
+    for(const tile of computerSequenceCopy){
         await flash(tile)
     }
+    canClick= true; // now user can click
+    info.textContent = "Your turn: " + computerSequence.length
+
 }
 
-flashRunner()
+// user clicking tiles
+
+startBtn.addEventListener("click", () => {
+    info.classList.remove("hidden")
+    info.textContent = "Wait for the computer!"
+    flashRunner(computerSequenceCopy)
+
+})
+
+for (const tile of tiles) {
+    tile.addEventListener("click", (e) => {
+        let currentTile = e.target;
+        if(!canClick) return;
+        const expectedTile = computerSequenceCopy.shift(); //returns removed element
+        if(expectedTile === currentTile){
+            if(computerSequenceCopy.length === 0){
+                computerSequence.push(getRandomTiles());
+                computerSequenceCopy = [...computerSequence]
+                flashRunner(computerSequenceCopy)
+            }
+        } 
+        else{
+            alert("Oops! Game Over")
+            location.reload();
+        }
+    
+    })
+}
