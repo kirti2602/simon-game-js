3032b7911ad535a8119f722c99ee5ff726b00381
Kirti Sharma <kirti2001sharma@gmail.com>
2023-01-27 13:02:44 +0530

    fix: fixed current score

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const info = document.querySelector(".info") // instructions
 const audioComputer  = document.querySelector(".audio-computer")
 const audioHuman  = document.querySelector(".audio-human")
 const highScore = document.querySelector(".high-score")
+const currentScore = document.querySelector(".current-score")
+
 let canClick = false; 
 let userHighScore;
 let tilesCount = tiles.length; 
@@ -23,8 +25,9 @@ const getRandomTiles = () => {
 const computerSequence = [getRandomTiles()]
 let userSequence= [...computerSequence] // change the name  to something more relatable/descriptive -- DONE 
 
-let userCurrentScore = computerSequence-1;
-highScore.innerHTML += `Current Score:- ${computerSequence}0 |${sessionStorage.highScore} 0` ;
+//implementing current score
+let userCurrentScore = computerSequence.length-1;
+highScore.innerHTML += `${sessionStorage.highScore}0` ;
 
 
 const flash = (tile) => {
@@ -52,8 +55,7 @@ const flashRunner = async (userSequence) => { // can take a simple name -- NOT S
         await flash(tile)
     }
     canClick= true; // now user can click
-    info.textContent = "Your turn! Press " + computerSequence.length + " times"
-
+    info.textContent = `Your turn! Press " ${computerSequence.length} times`
 }
 
 // user clicking tiles
@@ -77,6 +79,8 @@ for (const tile of tiles) {
         const expectedTile = userSequence.shift(); //returns removed element
         if (expectedTile === currentTile) {
             if (userSequence.length === 0) {
+                //updates the current score
+                currentScore.textContent = `Current Score:- ${computerSequence.length}0`
                 computerSequence.push(getRandomTiles());
                 userSequence = [...computerSequence]
                 flashRunner(userSequence)
@@ -95,4 +99,14 @@ for (const tile of tiles) {
 }
 
 
+/* suggested changes
+1) Resolve the issue of centring the div!
+2) Add the functionality of current round score along with Highscore
+3) Change the code where we use tiles.length. Make use of global variable instead of calculating length every-time.
+4) The computer starts playing the sequence immediately after I end up my turn, creating a little confusion. Can add a little pause there.
+5) Can remove id here https://github.com/kirti2602/simon-game-js/blob/78e65417b8f5f85c7ddf9551e141ddc336f86ada/index.html#L23 
+since it is blank. Or you can give the background color (red/blue/yellow/green) as ID instead of class.
+6) Row1 and row 2 are already given dislpay: flex in css (Refer here). But again they are explicitly given 
+flex class in html class(Refer here). Can avoid this duplicate styling!
+*/
 
